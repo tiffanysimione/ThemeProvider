ad0a0860ed0ba160454aa4bf3ae87c9b4a4a4387
tiffanysimione <simionetiffany1@gmail.com>
2023-04-09 13:25:32 -0400

    updating card media

--- src/components/styles/Card.styled.js
+++ src/components/styles/Card.styled.js
@@ -1,22 +1,33 @@
 import styled from 'styled-components'
 
 export const StyledCard = styled.div`
-background-image:linear-gradient(to right, #8000DB 0%, #1D88FE 100%);
+  background-image: linear-gradient(to right, #8000DB 0%, #1D88FE 100%);
   display: flex;
-  color:#FDFEFF;
+  color: #FDFEFF;
   align-items: center;
   border-radius: 15px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   margin: 40px 0;
-  padding: 60px;
+  padding: 20px;
   flex-direction: ${({ layout }) => layout || 'row'};
+  
   img {
-    width: 80%;
+    width: 60%;
+    margin-right: 40px;
   }
+  
   & > div {
     flex: 1;
   }
+  
   @media (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
+    padding: 20px 10px;
+    
+    img {
+      width: 100%;
+      margin-right: 0;
+      margin-bottom: 20px;
+    }
   }
 `
